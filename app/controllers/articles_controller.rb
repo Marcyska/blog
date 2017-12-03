c77d38f26252a373437839c6f8371b10cb03b085
Bartłomiej Chmiel <bartlomiejchmiel@MacBook-Air-Bartomiej.local>
2017-12-03 10:33:30 +0100

    Add destroy option for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,34 +1,49 @@
 class ArticlesController < ApplicationController
-	def index
-		@articles = Article.all.order(created_at: :asc)
-	end
+  def index
+    @articles = Article.all.order(created_at: :desc)
+  end
+  def new
+    @article = Article.new
+  end
+  def create
+    article_params = params.require(:article).permit(:title, :text)
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    article_params = params.require(:article).permit(:title, :text)
+    @article = Article.find(params[:id])
+    @article.attributes = article_params
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
 
-	def new
-		@article = Article.new
-	end
-
-	def create
-	article_params = params.require(:article).permit(:title, :text)
-	@article = Article.new(article_params)
-	if @article.save
-	redirect_to article_path(@article)
-else
-	render 'edit'
-end
-	end
-
-	def show
-		@article = Article.find(params[:id])
-end
-def edit
-	@article = Article.find(params[:id])
-end
-
-def update
-	article_params = params.require(:article).permit(:title, :text)
-	@article = Article.find(params[:id])
-	@article.attributes = article_params
-	@article.save
-	redirect_to article_path(@article)
+  def destroy
+	  @article = Article.find(params[:id])
+	  @article.destroy
+	  redirect_to articles_path
 	end
 end
+
+
+
+
+
+
+
+
+
+
