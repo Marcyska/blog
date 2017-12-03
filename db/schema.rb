93e03683dc7f441a3129be6d461e531b9e3455f3
Bartłomiej Chmiel <bartlomiejchmiel@MacBook-Air-Bartomiej.local>
2017-12-03 11:51:29 +0100

    Create model for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171202112606) do
+ActiveRecord::Schema.define(version: 20171203103341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20171202112606) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "manufacturers", id: :serial, force: :cascade do |t|
@@ -32,5 +41,6 @@ ActiveRecord::Schema.define(version: 20171202112606) do
     t.integer "manufacturer_id", null: false
   end
 
+  add_foreign_key "comments", "articles"
   add_foreign_key "products", "manufacturers", name: "products_manufacturer_id_fkey"
 end
