5c63cfdada315fdb3331294593875fd0be36c1dc
Bartłomiej Chmiel <bartlomiejchmiel@MacBook-Air-Bartomiej.local>
2017-12-16 15:52:14 +0100

    Add search tags

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,14 @@ class ArticlesController < ApplicationController
 before_action :find_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.all.order(created_at: :desc)
+ @articles = Article.all.order(created_at: :desc)
+    if params[:q].present?
+    @articles = @articles.select do |article|
+      article.tags.include?(params[:q])
+end
   end
+end
+
   def new
     @article = Article.new
   end
