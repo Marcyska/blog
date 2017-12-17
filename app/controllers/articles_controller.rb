e4cbba90e4bc53bcbddcb62a266fcbdf7c2e5107
Bartłomiej Chmiel <bartlomiejchmiel@MacBook-Air-Bartomiej.local>
2017-12-17 11:19:49 +0100

    Add one author can edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,20 @@
 class ArticlesController < ApplicationController
-
-before_action :find_article, only: [:show, :edit, :update, :destroy]
-
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_article, only: [:edit, :update, :destroy]
   def index
- @articles = Article.all.order(created_at: :desc)
+    @articles = Article.all.order(created_at: :desc)
     if params[:q].present?
-    @articles = @articles.select do |article|
-      article.tags.include?(params[:q])
-end
+      @articles = @articles.select do |article|
+        article.tags.include?(params[:q])
+      end
+    end
   end
-end
-
   def new
     @article = Article.new
   end
   def create
     @article = Article.new(article_params)
+    @article.author = current_user
     if @article.save
       redirect_to article_path(@article)
     else
@@ -24,13 +23,11 @@ end
   end
   def show
     @comment = Comment.new(commenter: session[:commenter])
-
   end
   def edit
-
+    
   end
   def update
-
     @article.attributes = article_params
     if @article.save
       redirect_to article_path(@article)
@@ -38,30 +35,20 @@ end
       render 'edit'
     end
   end
-
   def destroy
-
-	  @article.destroy
-	  redirect_to articles_path
-	end
-
-	private
-	def article_params
-	 params.require(:article).permit(:title, :text, :tags)
+    @article.destroy
+    redirect_to articles_path
+  end
+  private
+  def article_params
+    params.require(:article).permit(:title, :text, :tags)
+  end
+  def authorize_article
+    if @article.author != current_user
+      redirect_to articles_path, alert: "Idź sobie!"
+    end
+  end
+  def find_article
+    @article = Article.find(params[:id])
+  end
 end
-
-def find_article
-	@article = Article.find(params[:id])
-
-end
-end
-
-
-
-
-
-
-
-
-
-
